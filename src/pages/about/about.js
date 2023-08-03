f638286c4d5c8195038ca985e554e3b6801335fe
SageKyle <42160985+SageKyle@users.noreply.github.com>
2023-08-03 18:53:43 +0100

    Update about.js
    add more info to the about page

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,6 +1,8 @@
 export default function about() {
 	return `<section>
         <h2>About JavaScript SPA</h2>
-        <p>This is just a research project to test what it would be like to create a Single Page Application without using any framework or library</p>
+        <p>This is just a casual project to test what it would be like to create a Single Page Application without using any framework or library</p>
+	<p>While there might be a better way to do this, this was just an abrupt and comical attempt at doing the unusual; as such, I had no intention of spending too much time on it.</p>
+	<p>If this sparked any ideas in you, feel free to <a href='https://github.com/SageKyle/javascript-spa' target='_blank' rel='noreferrer'>check out the source code</a> and continue from where I stopped</p>
     </section>`
 }
